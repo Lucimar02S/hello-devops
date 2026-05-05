0d98a430c1cbf4372631c2e56a6db375a76c94b1
Lucimar02S <lucimarmay@gmail.com>
2026-05-05 20:25:57 -0300

    Fix formatting of hello function response

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
- return ' hello, turma Devops 2025!! '
+ return ' hello, turma Devops 2025!'
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=10000)
 
